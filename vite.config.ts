dde459f739a73c8622b1b7ea85adb3c21d57b5db
tadasgo <tadas@cornercasetech.com>
2022-07-27 09:58:03 +0300

    Setup main dependancies and rules
    - Eslint
    - Prettier
    - TS
    - Emotion
    - TanStack Query
    - React-Hook-form

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        presets: ['@emotion/babel-preset-css-prop'],
+      },
+    }),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+});
